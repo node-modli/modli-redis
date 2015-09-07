bab213f104ececc0d5318c488feb8d556d16492d
fluidbyte <kent@fluidbyte.net>
2015-09-07 12:33:48 -0400

    Create method working and tested

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// import Promise from 'bluebird';
+import Promise from 'bluebird';
 import redis from 'redis';
 
 /**
@@ -18,11 +18,43 @@ export default class {
     this.client = redis.createClient(config.port, config.host, config.opts);
     if (config.password) {
       this.client.auth(config.password, (err) => {
+        /* istanbul ignore if */
         if (err) {
           throw new Error(err);
         }
       });
     }
+  }
+
+  /**
+   * Creates a hash set based on body object
+   * @param {String|Number} key The key to use
+   * @param {Object} body The body/data to create in the hash
+   * @param {Number} expires The duration before expiration (in seconds)
+   * @param {String|Number} version The version of the model to validate
+   */
+  create (key, body, expires, version = false) {
+    return new Promise((resolve, reject) => {
+      const validationErrors = this.validate(body, version);
+      if (validationErrors) {
+        reject(validationErrors);
+      } else {
+        this.client.set(key, JSON.stringify(body), (err, reply) => {
+          /* istanbul ignore if */
+          if (err) {
+            reject(err);
+          } else {
+            if (expires) {
+              this.client.expire(key, expires, (e) => {
+                reject(e);
+                return;
+              });
+            }
+            resolve(reply);
+          }
+        });
+      }
+    });
   }
 
 }
--- test/src/index.spec.js
+++ test/src/index.spec.js
@@ -9,6 +9,23 @@ const config = {
   password: process.env.MODLI_REDIS_PASSWORD
 };
 
+const testRedis = new redis(config);
+
+// Mock validation method, this is automatically done by the model
+testRedis.validate = (body) => {
+  // Test validation failure by passing `failValidate: true`
+  if (body.failValidate) {
+    return { error: true };
+  }
+  // Mock passing validation, return null
+  return null;
+};
+
+// Mock sanitize method, this is automatically done by the model
+testRedis.sanitize = (body) => {
+  return body;
+};
+
 describe('redis', () => {
   describe('constructor', () => {
     it('throws error if invalid config passed', (done) => {
@@ -20,6 +37,34 @@ describe('redis', () => {
       expect(testConn).to.be.an.object;
       expect(testConn.client.address).to.equal(`${config.host}:${config.port}`);
       done();
+    });
+  });
+
+  describe('create', () => {
+    it('responds with failure if invalid data is passed', (done) => {
+      testRedis.create('foo', { failValidate: true}, false, 1)
+        .catch((err) => {
+          expect(err.error).to.be.true;
+          done();
+        });
+    });
+    it('throws an error if invalid params passed', (done) => {
+      expect(testRedis.create('fooFail', { foo: 'bar' }, 'fart')).to.throw;
+      done();
+    });
+    it('creates a new record in the store', (done) => {
+      testRedis.create('foo', { foo: 'bar' }, false, 1)
+        .then((res) => {
+          expect(res).to.equal('OK');
+          done();
+        });
+    });
+    it('creates a record that expires if numeric value set for expries param', (done) => {
+      testRedis.create('fooExpires', { foo: 'bar'}, 30)
+        .then((res) => {
+          expect(res).to.equal('OK');
+          done();
+        });
     });
   });
 });
