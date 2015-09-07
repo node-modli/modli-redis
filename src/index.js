7de4542f6e66a3f56d56a526adcd2d2e926328c3
fluidbyte <kent@fluidbyte.net>
2015-09-07 11:08:15 -0400

    Initial connection setup and tested

--- src/index.js
+++ src/index.js
@@ -6,6 +6,13 @@ import redis from 'redis';
  */
 export default class {
 
+  /**
+   * Sets up client for Redis connection
+   * @param {Object} config
+   * @property {String} config.host The host address or URI
+   * @property {Number} [config.port] The port of the Redis instance
+   * @property {String} [config.password] The password for the Redis instance
+   */
   constructor (config) {
     config.port = config.port || 6379;
     this.client = redis.createClient(config.port, config.host, config.opts);
--- test/src/index.spec.js
+++ test/src/index.spec.js
@@ -3,6 +3,23 @@
 import '../setup';
 import redis from '../../src/index';
 
+const config = {
+  host: process.env.MODLI_REDIS_HOST,
+  port: process.env.MODLI_REDIS_PORT,
+  password: process.env.MODLI_REDIS_PASSWORD
+};
+
 describe('redis', () => {
-  return redis;
+  describe('constructor', () => {
+    it('throws error if invalid config passed', (done) => {
+      expect(() => new redis({})).to.throw;
+      done();
+    });
+    it('connects to redis instance with valid config', (done) => {
+      const testConn = new redis(config);
+      expect(testConn).to.be.an.object;
+      expect(testConn.client.address).to.equal(`${config.host}:${config.port}`);
+      done();
+    });
+  });
 });
